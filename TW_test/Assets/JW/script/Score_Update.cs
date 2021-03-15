73aebf696609132f1f4c74b023f50a311febd163
ddkaise <78730857+usuk333@users.noreply.github.com>
2021-03-15 18:37:17 +0900

    03.15
    정우 파일 추가

---
+++ TW_test/Assets/JW/script/Score_Update.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Update : MonoBehaviour
+{
+    public static int score;
+    [SerializeField] private Text scoreLabel;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        score = 0;
+    }
+    
+    void Start()
+    {
+        scoreLabel.text = "Score : " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetAddScore()
+    {
+        score += 100;
+        scoreLabel.text = "Score : " + score.ToString();
+
+        GameObject.FindWithTag("Stage").GetComponent<Stage>().SetLevelUp();
+
+    }
+}
