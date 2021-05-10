15a04604866d9cb152b4ff1768055dfbb174b5d7
곽원준 <dnjswns-11@naver.com>
2021-05-10 20:09:47 +0900

    5.10
    5.10

--- TWtest/Assets/WJ/Script/PCTRLTest.cs
+++ TWtest/Assets/WJ/Script/PCTRLTest.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PCTRLTest : MonoBehaviour
 {
-    public float moveSpeed = 4f;
-    Vector3 forward;
-    Vector3 right;
+   
+
+    [SerializeField] private float moveSpeed = 4f;
+    private Vector3 forward;
+    private Vector3 right;
 
     private bool inputLeft = false;
     private bool inputRight = false;
@@ -17,80 +19,65 @@ public class PCTRLTest : MonoBehaviour
     private bool inputDownLeft = false;
     private bool inputDownRight = false;
 
-    public Camera centerCam;
-    public Camera backCam;
-    public Camera currentCam;
-    private bool isBack = false;
+    public bool InputLeft { get => inputLeft; set => inputLeft = value; }
+    public bool InputRight { get => inputRight; set => inputRight = value; }
+    public bool InputUp { get => inputUp; set => inputUp = value; }
+    public bool InputDown { get => inputDown; set => inputDown = value; }
+    public bool InputUpLeft { get => inputUpLeft; set => inputUpLeft = value; }
+    public bool InputUpRight { get => inputUpRight; set => inputUpRight = value; }
+    public bool InputDownLeft { get => inputDownLeft; set => inputDownLeft = value; }
+    public bool InputDownRight { get => inputDownRight; set => inputDownRight = value; }
+    public Vector3 Forward { get => forward; set => forward = value; }
+    public Vector3 Right { get => right; set => right = value; }
+
     // Start is called before the first frame update
     void Start()
     {
-        CameraChange(centerCam);
-    }
-    void CameraChange(Camera cam)
-    {
-        if (currentCam != null && currentCam.gameObject.activeSelf == true)
-        {
-            currentCam.gameObject.SetActive(false);
-        }
-        cam.gameObject.SetActive(true);
-        forward = cam.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
+        
 
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-        currentCam = cam;
     }
+   
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            if (currentCam == centerCam)
-                CameraChange(backCam);
-            else
-                CameraChange(centerCam);
-        }
-        if(inputLeft == true)
+   
+        if(InputLeft == true)
         {
             MoveLeft();
         }
-        else if(inputUp == true)
+        else if(InputUp == true)
         {
             MoveUp();
         }
-        else if(inputRight == true)
+        else if(InputRight == true)
         {
             MoveRight();
         }
-        else if(inputDown == true)
+        else if(InputDown == true)
         {
             MoveDown();
         }
-        else if(inputUpLeft == true)
+        else if(InputUpLeft == true)
         {
             MoveUpLeft();
         }
-        else if(inputUpRight == true)
+        else if(InputUpRight == true)
         {
             MoveUpRight();
         }
-        else if(inputDownLeft == true)
+        else if(InputDownLeft == true)
         {
             MoveDownLeft();
         }
-        else if(inputDownRight == true)
+        else if(InputDownRight == true)
         {
             MoveDownRight();
         }
     }
-    public void SetInputLeft()
-    {
-        inputLeft = !inputLeft;
-    }
 
     private void MoveLeft()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * -1;
         Vector3 direction = Vector3.Normalize(RightMovement);
 
         if (direction != Vector3.zero)
@@ -99,13 +86,10 @@ public class PCTRLTest : MonoBehaviour
             transform.position += RightMovement;
         }
     }
-    public void SetInputRight()
-    {
-        inputRight = !inputRight;
-    }
+
     private void MoveRight()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * 1;
         Vector3 direction = Vector3.Normalize(RightMovement);
 
         if (direction != Vector3.zero)
@@ -114,13 +98,10 @@ public class PCTRLTest : MonoBehaviour
             transform.position += RightMovement;
         }
     }
-    public void SetInputUp()
-    {
-        inputUp = !inputUp;
-    }
+
     private void MoveUp()
     {
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * 1;
         Vector3 direction = Vector3.Normalize(ForwardMovement);
 
         if (direction != Vector3.zero)
@@ -129,13 +110,10 @@ public class PCTRLTest : MonoBehaviour
             transform.position += ForwardMovement;
         }
     }
-    public void SetInputDown()
-    {
-        inputDown = !inputDown;
-    }
+
     private void MoveDown()
     {
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * -1;
         Vector3 direction = Vector3.Normalize(ForwardMovement);
 
         if (direction != Vector3.zero)
@@ -144,14 +122,11 @@ public class PCTRLTest : MonoBehaviour
             transform.position += ForwardMovement;
         }
     }
-    public void SetInputUpLeft()
-    {
-        inputUpLeft = !inputUpLeft;
-    }
+
     private void MoveUpLeft()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * -1;
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * 1;
         Vector3 FinalMovement = ForwardMovement + RightMovement;
         Vector3 direction = Vector3.Normalize(FinalMovement);
 
@@ -161,14 +136,11 @@ public class PCTRLTest : MonoBehaviour
             transform.position += FinalMovement;
         }
     }
-    public void SetInputUpRight()
-    {
-        inputUpRight = !inputUpRight;
-    }
+
     private void MoveUpRight()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * 1;
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * 1;
         Vector3 FinalMovement = ForwardMovement + RightMovement;
         Vector3 direction = Vector3.Normalize(FinalMovement);
 
@@ -179,14 +151,11 @@ public class PCTRLTest : MonoBehaviour
         }
     }
     
-    public void SetInputDownLeft()
-    {
-        inputDownLeft = !inputDownLeft;
-    }
+
     private void MoveDownLeft()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * -1;
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * -1;
         Vector3 FinalMovement = ForwardMovement + RightMovement;
         Vector3 direction = Vector3.Normalize(FinalMovement);
 
@@ -196,14 +165,11 @@ public class PCTRLTest : MonoBehaviour
             transform.position += FinalMovement;
         }
     }
-    public void SetInputDownRight()
-    {
-        inputDownRight = !inputDownRight;
-    }
+
     private void MoveDownRight()
     {
-        Vector3 RightMovement = right * moveSpeed * Time.smoothDeltaTime * 1;
-        Vector3 ForwardMovement = forward * moveSpeed * Time.smoothDeltaTime * -1;
+        Vector3 RightMovement = Right * moveSpeed * Time.smoothDeltaTime * 1;
+        Vector3 ForwardMovement = Forward * moveSpeed * Time.smoothDeltaTime * -1;
         Vector3 FinalMovement = ForwardMovement + RightMovement;
         Vector3 direction = Vector3.Normalize(FinalMovement);
 
